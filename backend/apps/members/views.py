b5356333e24b2550dc04ee772eb3621f1398b857
DmytroOLKH <dmytrowilk@gmail.com>
2026-08-27 22:22:32 +0200

    first views

---
+++ backend/apps/members/views.py
@@ -0,0 +1,8 @@
+
+from rest_framework import viewsets
+from .models import Member
+from .serializers import MemberSerializer
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
