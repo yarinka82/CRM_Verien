0c47d1981d237be996c0dff7d397a25717e865ed
Verien <dmytrowilk@gmail.com>
2026-08-31 22:32:20 +0200

    Fix production Docker config and ignore node_modules

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://backend:8000',
         changeOrigin: true,
       },
     },
